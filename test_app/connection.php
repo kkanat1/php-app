b04e37fb9afe6c8cce7f3e7b7e8890d5e2604d57
kanata <kanata13131@gmail.com>
2024-10-09 13:41:18 +0900

    feat: done til sectoin15

--- test_app/connection.php
+++ test_app/connection.php
@@ -24,3 +24,20 @@ function getAllRecords(){
     $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
     return $dbh->query($sql)->fetchAll();
 }
+
+// update
+function updateTodoData($post){
+    $dbh = connectPdo();
+    $sql = 'UPDATE todos SET content = "' . $post['content'] . '" WHERE id = ' . $post['id'];
+    $dbh->query($sql);
+}
+
+function getTodoTextById($id){
+    $dbh = connectPdo();
+    $sql = "SELECT * FROM todos WHERE deleted_at IS NULL AND id = {$id}";
+    // var_dump($sql);
+    // var_dump($dbh);
+    // exit;
+    $data = $dbh->query($sql)->fetch();
+    return $data['content'];
+}
--- test_app/functions.php
+++ test_app/functions.php
@@ -1,11 +1,31 @@
 <?php
 require_once('connection.php');
 
-function createData($post)
-{
-  createTodoData($post['content']);
-} 
-
 function getTodoList(){
   return getAllRecords();
+}
+
+function getSelectedTodo( $id ){
+  return getTodoTextByid($id);
+}
+
+function savePostedData($post)
+{
+    $path = getRefererPath();
+    switch ($path) {
+        case '/new.php':
+            createTodoData($post['content']);
+            break;
+        case '/edit.php':
+            updateTodoData($post);
+            break;
+        default:
+            break;
+    }
+}
+
+function getRefererPath()
+{
+    $urlArray = parse_url($_SERVER['HTTP_REFERER']);
+    return $urlArray['path'];
 }
